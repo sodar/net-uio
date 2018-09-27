6538f93956d3996d4a500a9ad02ed36dd1bcd32b
Dariusz Sosnowski <dsosnowski@dsosnowski.pl>
2018-09-28 00:38:25 +0200

    Remove match expressions from test program

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,13 @@ static CFG_PATH: &str = "/sys/class/uio/uio0/device/config";
 
 fn main() {
     let mut opts = OpenOptions::new();
-    let _uio = match opts.read(true).write(true).open(DEV_PATH) {
-        Ok(f) => f,
-        Err(e) => panic!(e),
-    };
+    let _uio = opts.read(true).write(true).open(DEV_PATH).unwrap();
 
     let mut opts = OpenOptions::new();
-    let mut cfg = match opts.read(true).write(true).open(CFG_PATH) {
-        Ok(f) => f,
-        Err(e) => panic!(e),
-    };
+    let mut cfg = opts.read(true).write(true).open(CFG_PATH).unwrap();
 
     let mut buffer = [0u8; 256];
-    let bytes = match cfg.read(&mut buffer) {
-        Ok(r) => r,
-        Err(e) => panic!(e),
-    };
+    let bytes = cfg.read(&mut buffer).unwrap();
     println!("[net-uio] Read {} bytes from uio0 config space", bytes);
 
     for i in 0..32 {
