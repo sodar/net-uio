54ce2d435047ce31e9a6a69cc9201f76a826e984
Dariusz Sosnowski <dsosnowski@dsosnowski.pl>
2018-09-30 21:36:12 +0200

    Enable LSC interrupt and wait for it
    Expanded example/exploratory program to:
    
    - map BAR0 of the NIC,
    - read a few NIC registers,
    - enable LSC interrupt,
    - wait for it by reading on `/dev/uio0`.
    
    It should work, theoretically.

--- notes.md
+++ notes.md
@@ -2,3 +2,9 @@
 
 
 - According to the spec 82540EM supports only the 32-bit PCI mode.
+- `/sys/class/uio/uio0/device` is symlinked to `/sys/bus/pci/devices/0000:00:04.0`.
+- `/sys/bus/pci/devices/0000:00:04.0/resource`:
+    - contains addresses of PCI resources,
+    - 1st cell - start address,
+    - 2nd cell - end address,
+    - 3rd cell - unknown
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 extern crate byteorder;
+extern crate libc;
 
 use byteorder::{LittleEndian, ReadBytesExt};
+use libc::*;
 use std::io::Read;
 use std::fs::OpenOptions;
+use std::os::unix::io::IntoRawFd;
 
 static DEV_PATH: &str = "/dev/uio0";
 static CFG_PATH: &str = "/sys/class/uio/uio0/device/config";
+static BAR0_PATH: &str = "/sys/class/uio/uio0/device/resource0";
 
 fn read_u16(buf: &[u8], off: usize) -> u16 {
     (&buf[off .. off + 2]).read_u16::<LittleEndian>().unwrap()
@@ -29,7 +33,7 @@ fn print_config_space(buf: &[u8; 256]) {
 
 fn main() {
     let mut opts = OpenOptions::new();
-    let _uio = opts.read(true).write(true).open(DEV_PATH).unwrap();
+    let mut uio = opts.read(true).write(true).open(DEV_PATH).unwrap();
     println!("net-uio: Opened {}", DEV_PATH);
 
     let mut opts = OpenOptions::new();
@@ -59,4 +63,69 @@ fn main() {
 
     println!("net-uio: bar_reg, low = {:#010x}, high = {:#010x}",
         bar_reg_low, bar_reg_high);
+
+    let bar0 = OpenOptions::new().read(true).write(true)
+        .open(BAR0_PATH)
+        .expect("Unable to open PCI resource file");
+    let bar0_fd = bar0.into_raw_fd();
+
+    let addr: *mut c_void = 0 as *mut c_void;
+    let len: size_t = 131072;
+    let prot = PROT_READ | PROT_WRITE;
+    let flags = MAP_SHARED;
+    let fd: c_int = bar0_fd;
+    let offset: off_t = 0;
+
+    let m: *mut c_void;
+    unsafe {
+        m = mmap(addr, len, prot, flags, fd, offset)
+    }
+    println!("net-uio: mmap result = {:?}", m);
+
+    let bar0_ptr = m as *mut u32;
+
+    unsafe {
+        let ctrl_reg = bar0_ptr.offset(0);
+        println!("net-uio: CTRL pointer = {:?}", ctrl_reg);
+        println!("net-uio: CTRL = {:#010x}", *ctrl_reg);
+    }
+
+    unsafe {
+        let status_reg = bar0_ptr.offset(2);
+        println!("net-uio: STATUS pointer = {:?}", status_reg);
+        println!("net-uio: STATUS = {:#010x}", *status_reg);
+    }
+
+    // NOTE: Manipulating Interrupt Mask Set/Read register
+    unsafe {
+        let ims_reg = bar0_ptr.offset(52);
+        println!("net-uio: IMS pointer = {:?}", ims_reg);
+        println!("net-uio: IMS = {:#010x}", *ims_reg);
+
+        let ims_reg_value: u32 = 0b100;
+        *ims_reg = ims_reg_value;
+        println!("net-uio: IMS setting LSC bit (link status change)");
+        println!("net-uio: IMS = {:#010x}", *ims_reg);
+    }
+
+    // NOTE: Reading Interrupt Mask Cause Read Register
+    unsafe {
+        let icr_reg = bar0_ptr.offset(48);
+        println!("net-uio: ICR pointer = {:?}", icr_reg);
+        println!("net-uio: ICR = {:#010x}", *icr_reg);
+        println!("net-uio: ICR = {:#010x}", *icr_reg);
+
+        //let icr_reg_value: u32 = 0b100;
+        //*icr_reg = icr_reg_value;
+        //println!("net-uio: ICR, ");
+    }
+
+    let mut buffer = [0u8; 4];
+    assert_eq!(uio.read(&mut buffer).unwrap(), 4);
+
+    unsafe {
+        assert_eq!(munmap(m, 131072), 0);
+    }
+
+    unsafe { assert_eq!(close(bar0_fd), 0); }
 }
